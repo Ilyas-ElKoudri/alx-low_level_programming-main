bf0259ebbd507bec2d7e18ce9e7cf741cbfb7b1a
Ilyas-ElKoudri <125877228+Ilyas-ElKoudri@users.noreply.github.com>
2023-12-05 15:34:19 +0100

    Alx-Swe 0x09-static_libraries

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,3 +1,19 @@
-#include "main.h"
+#include "holberton.h"
+
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
 int _strlen(char *s)
-{}
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
 void _puts(char *str)
-{}	
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
