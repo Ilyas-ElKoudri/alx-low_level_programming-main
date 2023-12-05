9043db60c662b19ec23bc5625e01417e3384e7f9
Ilyas-ElKoudri <125877228+Ilyas-ElKoudri@users.noreply.github.com>
2023-12-05 15:58:44 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,40 +10,40 @@
  */
 void print_opcodes(char *a, int n)
 {
-int i;
+	int i;
 
-for (i = 0; i < n; i++)
-{
-printf("%.2hhx", a[i]);
-if (i < n - 1)
-printf(" ");
-}
-printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 
 }
 
 /**
  * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
  *
- * Return: O
+ * Return: always O
  */
 int main(int argc, char **argv)
 {
-int a;
+	int n;
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-a = atoi(argv[1]);
-if (a < 0)
-{
-printf("Error\n");
-exit(2);
-}
-print_opcodes((char *)&main, a);
-return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,30 @@
 #include "function_pointers.h"
+
 /**
- * int_index - searches for an integer
- * @array: array with data
- * @size: size of the array
- * @cmp: function pointer
- * Return: count of integer
- */
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: ...
+  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int i = 0;
 
-if (size <= 0)
-return (-1);
-if (array == NULL || cmp == NULL)
-return (-1);
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
 
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) == 1)
-return (i);
-}
-return (-1);
+				i++;
+			}
+		}
+	}
+
+	return (-1);
 }
